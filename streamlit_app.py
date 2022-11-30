e6cb6241501eca6f59cbffc3d9a76f2fbf08b400
pavankumarbag <119449950+pavankumarbag@users.noreply.github.com>
2022-11-30 10:32:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,11 @@ st.header('Input Parameters')
 
 number_1 = st.number_input('Enter first number')
 number_2 = st.number_input('Enter second number')
-number_3 = st.number_input('Enter third number')
+#number_3 = st.number_input('Enter third number')
     
 
 data = {'First_Number': number_1,
         'Second_Number': number_2,
-        'Third_Number': number_3
         }
 
 
@@ -25,10 +24,10 @@ st.write(data)
 
 #Getting largest
 
-numbers = [data['First_Number'],data['Second_Number'],data['Third_Number']]
+numbers = [data['First_Number'],data['Second_Number']]
 numbers.sort()
 
 #Output
 
-st.subheader('Greatest number among the given inputs is:')
-st.write(numbers[2])
+st.subheader('Subtraction of two nos is:')
+st.write(numbers[1]-number[2)
